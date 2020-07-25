a6493c14d0073c47e6fa24177d08d3597ead2cb2
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-07-25 19:39:00 +0400

    feat: create portfolio card basic layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 console.log('connected');
 import './sass/style.scss';
+
+import Swiper from './swiper/swiperAPI';
+
+const swiper = new Swiper('.swiper-container', {
+ 
+  slidesPerView: 1,
+  spaceBetween: 10,
+  slidesPerGroup: 1,
+  loop: false,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 3,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
