e647b3e320ccfffe306f99b2df9482afe208f949
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-08-18 15:19:14 +0400

    feat: update header and styles

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = (env, options) => {
           ],
         },
         {
-          test: /\.(png|svg|jpe?g|gif)$/,
+          test: /\.(png|svg|jpe?g|JPG|gif)$/,
           use: [
             {
               loader: 'file-loader',
